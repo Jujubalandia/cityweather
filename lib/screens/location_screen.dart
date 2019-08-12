132ddb252436ab27d246f15da92c1bd60e8123f9
brunormoura <brunormoura@gmail.com>
2019-08-12 07:25:02 -0300

    City TextInput Field Style Decoration

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:clima/services/weather.dart';
+import 'package:clima/screens/city_screen.dart';
 
 class LocationScreen extends StatefulWidget {
   final locationWeather;
@@ -25,13 +26,12 @@ class _LocationScreenState extends State<LocationScreen> {
 
   void updateUI(dynamic weatherData) {
     setState(() {
-
       if (weatherData == null) {
         temperature = 0;
         weatherIcon = 'Error';
-        weatherMessage = 'Unable to get weather data'
+        weatherMessage = 'Unable to get weather data';
         cityName = '';
-        return ;        
+        return;
       }
       double temp = weatherData['main']['temp'];
       temperature = temp.toInt();
@@ -74,7 +74,14 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) {
+                          return CityScreen();
+                        }),
+                      );
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 const kApiKey = '77f7d546f692f1c1f219e79a808b520a';
 const KOpenUrlWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
@@ -21,4 +20,23 @@ const kButtonTextStyle = TextStyle(
 
 const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
+);
+
+const kTextFieldInputCityDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0),
+    ),
+    borderSide: BorderSide.none,
+  ),
 );
